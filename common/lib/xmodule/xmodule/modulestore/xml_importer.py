55a8b4d358b70cb742ed42a9fc315afcd6bed5b2
Oleg Marshev <oleh.marshev@gmail.com>
2014-05-08 21:03:44 +0300

    Add srt to type map.

--- common/lib/xmodule/xmodule/modulestore/xml_importer.py
+++ common/lib/xmodule/xmodule/modulestore/xml_importer.py
@@ -37,6 +37,7 @@ def import_static_content(
     verbose = True
 
     mimetypes.add_type('application/json', '.sjson')
+    mimetypes.add_type('application/x-subrip', '.srt')
     mimetypes_list = mimetypes.types_map.values()
 
     for dirname, _, filenames in os.walk(static_dir):
